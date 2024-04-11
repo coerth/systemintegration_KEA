ed893220edb2599cbc3661e16ef2faac6499ea76
Morten Bendeke <mbendeke@hotmail.com>
2024-04-11 12:28:49 +0200

    audio video and images

--- 17._Multipart-Form/node/app.js
+++ 17._Multipart-Form/node/app.js
@@ -5,7 +5,47 @@ const app = express(urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 8080;
 
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "./uploads/" });
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(req);
+    console.log(file);
+
+    cb(null, './uploads')
+  },
+  filename: (req, file, cb) => {
+    //const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    // extract the file extension
+    const fileExtension = file.originalname.split('.')[1];
+
+    //Anders løsning
+    const uniqueFileName = `${uniquePrefix}__${file.originalname}`;
+    cb(null, uniqueFileName)
+  }
+});
+
+function fileFilter(req, file, cb) {
+  const allowedTypes = ["image/jpeg", "image/jpg", "image/png"];  
+
+  if (!allowedTypes.includes(file.mimetype)) {
+    const error = new Error("Wrong file type: " + file.mimetype);
+    error.code = "LIMIT_FILE_TYPES";
+    return cb(error, false);
+  }
+
+  cb(null, true);
+}
+
+const upload = multer({
+   storage: storage, 
+   limits: {
+    fileSize: 10 * 1024 * 1024 // 10 MB
+   },
+   fileFilter
+  })
 
 app.post("/form", (req, res) => {
     console.log(req.body);
--- 17._Multipart-Form/python-fastapi/main.py
+++ 17._Multipart-Form/python-fastapi/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, File, UploadFile
+import aiofiles
+from typing import Optional
 
 app = FastAPI()
 
@@ -6,3 +8,33 @@ app = FastAPI()
 def basic_form(username: str = Form(...), password: str = Form(...)):
     print(username, password)
     return {}
+
+# @app.post("/fileform")
+# def file_form(file:bytes =File(...)):
+#     with open('file', 'wb') as f:
+#         f.write(file)
+
+#         return {"message": "File uploaded successfully"}
+    
+# @app.post("/fileform")
+# def file_form(file: UploadFile = File(...)):
+#     with open('file', 'wb') as f:
+#         f.write(file.file.read())
+
+#         return {"message": "File uploaded successfully"}
+
+# @app.post("/fileform")
+# async def file_form(file: UploadFile = File(...)):
+#     safe_filename = file.filename.replace('/', '_').replace('\\', '_')
+#     with open(safe_filename, 'wb') as f:
+#         # walrus operator
+#         while content:= await file.read(1024): #read 1kb at a time
+#             f.write(content)
+
+@app.post("/fileform")
+async def file_form(file: UploadFile = File(...), description: Optional[str] = Form(None)):
+    safe_filename = file.filename.replace('/', '_').replace('\\', '_')
+    with aiofiles.open(safe_filename, 'wb') as f:
+        # walrus operator
+        while content:= await file.read(1024): #read 1kb at a time
+            await f.write(content)
