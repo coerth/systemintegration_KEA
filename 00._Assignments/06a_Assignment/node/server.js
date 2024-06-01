cd71a1b31e93e069cee54ed6b982c2ec81053086
Morten Bendeke <mbendeke@hotmail.com>
2024-06-01 21:25:29 +0200

    small changes

--- 00._Assignments/06a_Assignment/node/server.js
+++ 00._Assignments/06a_Assignment/node/server.js
@@ -1,26 +1,34 @@
 import { WebSocketServer } from "ws";
 
-
 const PORT = process.env.PORT || 8080;
 
-const wss = new WebSocketServer({ port: PORT});
+const wss = new WebSocketServer({ port: PORT });
 
 wss.on("connection", (ws) => {
     console.log("Client connected", wss.clients.size);
 
-    
-
     ws.on("close", () => {
         console.log("Client disconnected", wss.clients.size);
-
-        
     });
 
     ws.on("message", (message) => {
-        console.log(`Received message => ${message}`,);
+        console.log(`Received message => ${message}`);
         ws.send("Hello! Message from server");
+
+        // Broadcast the message to all clients
         wss.clients.forEach((client) => {
-            client.send(`Client connected ${wss.clients.size}`);
+            // Check if the client is ready to receive messages
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(`Client connected ${wss.clients.size}`);
+            }
         });
     });
+
+    ws.on("error", (err) => {
+        console.log(`Server error: ${err}`);
+    });
+});
+
+wss.on("error", (err) => {
+    console.log(`Server error: ${err}`);
 });
